f4e45190d91ef7614c5e6c325f05240db400a3dd
Matt Willshire <matthewwillshire@Apollo.local>
2014-02-03 23:02:46 +0000

    Small additions to home_page mappings

--- features/support/pages/home_page.rb
+++ features/support/pages/home_page.rb
@@ -3,12 +3,22 @@ class HomePage
   include FigNewton
 
   page_url("#{FigNewton.base_url}")
+  link(:login, :id => "ui-id-3")
+  text_field(:name, :name => "board")
+  text_field(:password, :name => "password")
+  button(:submit, :type => "submit")
 
   def login_to_owned_board
-    pending
+    #login
+    #set name
+    #set password
+    #submit
   end
   
   def login_to_unowned_board
-    pending
+    #login
+    #set name
+    #set password
+    #submit
   end
 end
